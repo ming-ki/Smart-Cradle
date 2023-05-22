e159abda031ae2401abf9c8d05964a11cc7e2139
ming-ki <74133495+ming-ki@users.noreply.github.com>
2023-05-22 16:03:36 +0900

    Update LED_Control_basic.ino

--- LED Control/LED_Control_basic.ino
+++ LED Control/LED_Control_basic.ino
@@ -1,29 +1,51 @@
 #include <Adafruit_NeoPixel.h>
 
-#define LED_PIN 5        // LED가 연결된 핀 번호
-#define LED_COUNT 36     // 연결된 LED 개수
-#define PIR_PIN D4       // PIR 센서가 연결된 핀 번호
+const int pinPIRM = 2;
+int val = 0;
+bool motionDetected = false;
+bool ledsOn = false;
 
-Adafruit_NeoPixel pixels(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+#define LED_PIN     6
+#define LED_COUNT   36
+
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  pixels.begin();
-  pinMode(PIR_PIN, INPUT);  // PIR 센서의 핀을 입력으로 설정합니다.
+  pinMode(pinPIRM, INPUT);
+  Serial.begin(9600);
+  strip.begin();
+  strip.show(); // 초기 LED 상태를 설정
 }
 
 void loop() {
-  int pirValue = digitalRead(PIR_PIN);
-  Serial.println(pirValue);
-  if(pirValue == HIGH) {
-    for(int i=0; i<LED_COUNT; i++) {
-      pixels.setPixelColor(i, pixels.Color(255, 255, 255)); // 모든 LED를 흰색으로 설정합니다.
+  val = digitalRead(pinPIRM);
+  Serial.println(val);
+  
+  if (val == HIGH && !motionDetected) {
+    motionDetected = true;
+    
+    if (!ledsOn) {
+      // 모션 감지 시 모든 LED를 켬
+      for (int i = 0; i < LED_COUNT; i++) {
+        strip.setPixelColor(i, 255, 255, 255);
+      }
+      strip.show();
+      ledsOn = true;
     }
-    pixels.show();                                     // 설정한 색으로 LED를 표시합니다.
-  }else{
-    for(int i=0; i<LED_COUNT; i++) {
-    pixels.setPixelColor(i, pixels.Color(0, 0, 0));   // 모든 LED를 끕니다.
   }
-  pixels.show();
+  
+  if (val == LOW && motionDetected) {
+    motionDetected = false;
+    
+    if (ledsOn) {
+      // 모션 감지가 해제되면 모든 LED를 끔
+      for (int i = 0; i < LED_COUNT; i++) {
+        strip.setPixelColor(i, 0, 0, 0);
+      }
+      strip.show();
+      ledsOn = false;
+    }
   }
-  delay(100);    // 0.1초 대기
+  
+  delay(100);
 }
