9ecc94fe070f57e2cc354da7e368913c492d9e35
ming-ki <74133495+ming-ki@users.noreply.github.com>
2023-05-23 19:12:49 +0900

    Update Send_dht11_toMobius.ino

--- Temperature and humidity/Send_dht11_toMobius.ino
+++ Temperature and humidity/Send_dht11_toMobius.ino
@@ -1,6 +1,7 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <DHT.h>
+#include <ArduinoJson.h>
 
 const char* ssid = "ubicomp"; // Wi-Fi SSID
 const char* password = "ubicomp407"; // Wi-Fi Password
@@ -8,14 +9,16 @@ const char* server = "203.253.128.177"; // 모비우스 서버 IP 주소
 const int port = 7579; // 모비우스 서버 포트
 const String cnt = "Temperature";
 const String ae = "CapstonDesign"; // 모비우스에서 사용할 AE 이름
+const String cnt2 = "State";
 
 #define DHTPIN 5 // DHT11 데이터 핀
 #define DHTTYPE DHT11 // DHT11 사용
 
 DHT dht(DHTPIN, DHTTYPE); // DHT11 객체 생성
 
-int hum_pin = 0; // 가습기 핀 D8
+int hum_pin = 0; // 가습기 핀
 float t, h;
+String con = "0"; // 초기 가습기 상태 값
 
 void setup() {
   Serial.begin(115200);
@@ -48,9 +51,14 @@ void loop() {
       return;
     }
 
-    String payload = "{\"m2m:cin\": {\"con\": \"" + String(temp) + "," + String(hum) + "," + String(waterSensorVal)+"\"}}"; // 전송할 데이터
+    // JSON 데이터 생성
+    DynamicJsonDocument doc(512); // JSON 문서 크기를 512바이트로 설정
+    doc["m2m:cin"]["con"] = String(temp) + "," + String(hum) + "," + String(waterSensorVal);
+    String payload;
+    serializeJson(doc, payload); // JSON 데이터를 문자열로 직렬화
 
     HTTPClient http;
+    HTTPClient http2;
 
     // HTTP POST 요청 설정
     http.begin("http://" + String(server) + ":" + String(port) + "/Mobius/" + ae + "/" + cnt);
@@ -62,6 +70,15 @@ void loop() {
     String response = http.getString(); // 서버 응답 수신
     http.end(); // HTTP 연결 종료
 
+    // State 데이터를 받아옴
+    http2.begin("http://" + String(server) + ":" + String(port) + "/Mobius/" + ae + "/" + cnt2 + "/latest");
+    http2.addHeader("Accept", "application/json");
+    http2.addHeader("X-M2M-RI", "12345");
+    http2.addHeader("X-M2M-Origin", ae);
+    int httpCode2 = http2.GET();
+    String response2 = http2.getString();
+    http2.end();
+
     if (httpCode == 201) { // HTTP 응답 코드가 201인 경우
       Serial.println("Data sent to Mobius");
       Serial.println(response);
@@ -71,8 +88,18 @@ void loop() {
       Serial.println(response);
     }
 
-    // 습도가 36% 미만이면 가습기를 작동시킴
-    if (hum <= 27) {
+    if (httpCode2 == 200) {
+      Serial.println("Data received from Mobius:");
+
+      DynamicJsonDocument doc2(512); // JSON 문서 크기를 512바이트로 설정
+      deserializeJson(doc2, response2);
+
+      con = doc2["m2m:cin"]["con"].as<String>();
+      Serial.println("State: " + con);
+    }
+
+    // 습도가 36% 미만이거나 con 값이 "1"이면 가습기를 작동시킴
+    if (hum <= 36 || con == "1") {
       digitalWrite(hum_pin, HIGH); // 가습기 켬
     } else {
       digitalWrite(hum_pin, LOW); // 가습기 끔
